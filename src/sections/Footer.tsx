08b74a8eeb1ef9f1cb2abf2ca856146a773de721
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-08-06 15:15:00 +0530

    implemented logout functionality

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,11 +1,21 @@
+import { signOut } from "firebase/auth";
 import { MdOutlinePowerSettingsNew } from "react-icons/md";
+import { firebaseAuth } from "../utils/FirebaseConfig";
+import { useAppDispatch } from "../app/hooks";
+import { setToast, setUserStatus } from "../app/slices/AppSlice";
 function Footer() {
+  const dispatch = useAppDispatch();
+  const handLogout = () => {
+    signOut(firebaseAuth);
+    dispatch(setUserStatus(undefined));
+    dispatch(setToast("Logged out successfully from Firebase."));
+  };
   return (
     <footer>
       <div className="block"></div>
       <div className="data"></div>
       <div className="block">
-        <MdOutlinePowerSettingsNew />
+        <MdOutlinePowerSettingsNew onClick={handLogout} />
       </div>
     </footer>
   );
